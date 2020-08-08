a79668582783d71a17230c602a09051e5f929dfb
Nihal Bhovare <nihalblog25@gmail.com>
2020-08-08 19:30:57 +0530

    m

--- includes/conn.php
+++ includes/conn.php
@@ -2,7 +2,7 @@
      define('DB_SERVER','localhost');
      define('DB_USER','shreeswa_shreeswa');
      define('DB_PASS' ,'Databaseforshree@2020');
-     define('DB_NAME', 'shreeswa_shree');
+     define('DB_NAME', 'shreeswa_shreeswa');
      
      $conn = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
      // Check connection
