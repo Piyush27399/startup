39710907131202e1489077ef85a76b366069ca64
Nihal Bhovare <nihalblog25@gmail.com>
2020-08-08 19:10:14 +0530

    Monor fixes

--- includes/conn.php
+++ includes/conn.php
@@ -1,8 +1,8 @@
 <?php
      define('DB_SERVER','localhost');
-     define('DB_USER','nihal');
-     define('DB_PASS' ,'root');
-     define('DB_NAME', 'shree');
+     define('DB_USER','shreeswa_shreeswa');
+     define('DB_PASS' ,'Databaseforshree@2020');
+     define('DB_NAME', 'shreeswa_shree');
      
      $conn = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
      // Check connection
